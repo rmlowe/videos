aa7d11ebfd775a3635ce3c16e6300a2fdb22a4e5
Robert Lowe <robertmlowe@rmlowe.com>
2018-12-24 01:46:19 +0800

    Searching for Videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyDWpOqpJvGq6xu-zbWu8Eb6yp3yNagjcWI';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
